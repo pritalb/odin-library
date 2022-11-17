2a90c22bab95a5125eb15db963b5028f58dc641e
pritalb <pritalbamnodkar@gmail.com>
2022-11-17 14:03:14 +0530

    implemented the methods to add and remove books from library

--- script.js
+++ script.js
@@ -9,6 +9,37 @@ function Book(title, id, author, numOfPages, isRead) {
     this.isRead = isRead;
 }
 
-function addBooktoLibrary() {
-
+function addBooktoLibrary(title, author, numOfPages, isRead) {
+    let book = new Book(title, available_book_id, author, numOfPages, isRead);
+    available_book_id += 1;
+    library.push(book);
 }
+
+function removeBookFromLibrary(id) {
+    for (let index = 0; index < library.length; index++) {
+        const book = library[index];
+
+        if (book.id == id) {
+            library.splice(index, 1);
+            break;
+        }
+    }
+}
+
+function displayLibrary() {
+    library.forEach(book => {
+        console.log(book);
+    })
+}
+
+function main() {
+    addBooktoLibrary('book name', 'dude', 15, false);
+    addBooktoLibrary('great book', 'other dude', 156, true);
+    addBooktoLibrary('some book name', 'dude', 215, false);
+    addBooktoLibrary('long book name', 'professor dude', 615, false);
+
+    // console.log(library);
+    removeBookFromLibrary(2);
+    displayLibrary();
+}
+main()

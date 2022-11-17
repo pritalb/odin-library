283428f304c5f1aa3659b28fb4d16005ce722db2
pritalb <pritalbamnodkar@gmail.com>
2022-11-17 13:11:48 +0530

    implemented the constructor for Book object

---
+++ script.js
@@ -0,0 +1,14 @@
+let library = []
+let available_book_id = 1
+
+function Book(title, id, author, numOfPages, isRead) {
+    this.title = title;
+    this.id = id;
+    this.author = author;
+    this.numOfPages = numOfPages;
+    this.isRead = isRead;
+}
+
+function addBooktoLibrary() {
+
+}

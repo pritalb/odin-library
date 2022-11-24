6c1e5b333bc1f24931ef59360ad30be2905956d7
pritalb <47136856+pritalb@users.noreply.github.com>
2022-11-24 18:35:21 +0530

    cleaned up script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let library = [];
 let available_book_id = 1;
-// let show_add_book_form = true;
 
 function Book(title, id, author, numOfPages, isRead) {
     this.title = title;
@@ -102,33 +101,7 @@ function displayLibrary() {
     })
 }
 
-// function toggle_add_book_form() {
-//     let add_book_form = document.querySelector('.add-book-form');
-
-//     if (show_add_book_form) {
-//         add_book_form.style.display = 'flex';
-//     } else {
-//         add_book_form.style.display = 'none';
-//     }
-//     show_add_book_form = !show_add_book_form;
-// }
-
 function main() {
     document.querySelector('.add-book-form-submit-btn').addEventListener('click', createBook);
-    // document.querySelector('.add-book-btn').addEventListener('click', toggle_add_book_form);
-
-    // toggle_add_book_form();
-    addBooktoLibrary('book name', 'dude', 15, false);
-    addBooktoLibrary('book name', 'dude', 15, false);
-    addBooktoLibrary('book name', 'dude', 15, false);
-    addBooktoLibrary('book name', 'dude', 15, false);
-    addBooktoLibrary('book name', 'dude', 15, false);
-    // addBooktoLibrary('great book', 'other dude', 156, true);
-    // addBooktoLibrary('some book name', 'dude', 215, false);
-    // addBooktoLibrary('long book name', 'professor dude', 615, false);
-
-    // console.log(library);
-    // removeBookFromLibrary(2);
-    // displayLibrary();
-}
+ }
 main()
